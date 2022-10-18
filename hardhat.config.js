8fa061c78a9267b8ca15e9fa54b3c1a2ffc851d8
Rubaisa <rubaisajawed8@gmail.com>
2022-10-18 20:25:11 +0500

    deployed contract on testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const PK = process.env.PRIVATE_KEY
+const URL = process.env.RPC_URL
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    mumbai: {
+      url: URL,
+      accounts: [PK]
     },
   },
   solidity: "0.8.7",
