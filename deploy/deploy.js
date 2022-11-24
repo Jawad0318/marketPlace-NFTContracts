6500abf73788f17d970f55a58b4ee149250c6534
Rubaisa <rubaisajawed8@gmail.com>
2022-11-24 17:17:01 +0500

    Updated deployment file

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -8,25 +8,13 @@ async function main() {
     const Marketplace = await ethers.getContractFactory('NFTDepotMarketplace');
     const marketplace = await Marketplace.deploy('NFTDepot');
 
-    // const NFT = await ethers.getContractFactory('testNFT');
-    // const nft = await NFT.deploy();
-
-    await marketplace.deployed();
-    // await nft.deployed();
-
     const data = {
         address: marketplace.address,
         abi: JSON.parse(marketplace.interface.format('json'))
     }
 
-    // const nftdata = {
-    //     address: nft.address,
-    //     abi: JSON.parse(nft.interface.format('json'))
-    // }
-
     // This writes the ABI and Address to the marketplace.json
     fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
-    // fs.writeFileSync('./src/NFT.json', JSON.stringify(nftdata))
 }
 
 main()
