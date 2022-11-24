5444f7f3802577a0ce994d1cf88967a5b0827a10
Rubaisa <rubaisajawed8@gmail.com>
2022-11-24 17:20:45 +0500

    Updated etherscan details

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,32 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+require('@nomiclabs/hardhat-etherscan');
 
 const PK = process.env.PRIVATE_KEY
 const URL = process.env.RPC_URL
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  solidity: '0.8.7',
   networks: {
     hardhat: {
       chainId: 1337
     },
     mumbai: {
-      url: URL,
-      accounts: [PK]
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
-  solidity: "0.8.7",
+  etherscan: {
+    apiKey: process.env.APIKEY,
+  },
+  customChains: [
+    {
+      network: 'polygonMumbai',
+      chainId: 80001,
+      urls: {
+        apiURL: 'https://api-testnet.polygonscan.com',
+        browserURL: 'https://mumbai.polygonscan.com',
+      },
+    },
+  ],
 };
